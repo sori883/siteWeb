38911d932a52a0d158ff0c08749a6813f46fbd95
sori883 <service@dlme.biz>
2022-08-28 04:15:35 +0900

    labelがないときにlabel要素を表示しないように変更

--- src/components/form/fieldWrapper.tsx
+++ src/components/form/fieldWrapper.tsx
@@ -16,9 +16,11 @@ export const FieldWrapper = (props: FieldWrapperProps): JSX.Element => {
   const { label, className, error, children } = props;
   return (
     <div className='form-control w-full'>
-      <label className={clsx('label', className)}>
-        <span className='label-text'>{label}</span>
-      </label>
+      {label && (
+        <label className={clsx('label', className)}>
+          <span className='label-text'>{label}</span>
+        </label>
+      )}
       <div>{children}</div>
       {error?.message && (
         <div role="alert" aria-label={error.message} className="text-sm font-semibold text-red-500">
